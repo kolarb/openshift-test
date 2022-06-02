b8df9a337b5a047319118a745c6875ae965ba1e7
kolarb <benjamin.kolar@gmail.com>
2022-06-02 09:52:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,17 @@ $link = mysqli_connect("mariadb:3306", "t2user", "t2!user", "sampledb");
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+$sql = "INSERT INTO persons (first_name, last_name, email) VALUES
+            ('John', 'Rambo', 'johnrambo@mail.com'),
+            ('Clark', 'Kent', 'clarkkent@mail.com'),
+            ('John', 'Carter', 'johncarter@mail.com'),
+            ('Harry', 'Potter', 'harrypotter@mail.com')";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
  
 // Attempt select query execution
 $sql = "SELECT * FROM persons";
