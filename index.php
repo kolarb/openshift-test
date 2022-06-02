1a22e2208fbaae00640ac401c723e3a998e640b1
kolarb <benjamin.kolar@gmail.com>
2022-06-02 09:53:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,19 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
+// Attempt create table query execution
+$sql = "CREATE TABLE persons(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(70) NOT NULL UNIQUE
+)";
+if(mysqli_query($link, $sql)){
+    echo "Table created successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
 $sql = "INSERT INTO persons (first_name, last_name, email) VALUES
             ('John', 'Rambo', 'johnrambo@mail.com'),
             ('Clark', 'Kent', 'clarkkent@mail.com'),
